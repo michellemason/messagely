3951af09e8a7a3125a202b686fa6e537e14072b4
Michelle Mason <michellemason95@yahoo.com>
2022-10-15 14:19:11 +0100

    completed user model

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 const express = require("express");
 const cors = require("cors");
-const { authenticateJWT } = require("./middleware/auth");
+const { authenticateJWT, ensureLoggedIn, ensureCorrectUser } = require("./middleware/auth");
 
 const ExpressError = require("./expressError")
 const app = express();
